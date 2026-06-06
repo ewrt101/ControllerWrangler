643aeb0e8d405a759a99112359922f906c843d9e
Ewrt <dan@radnewt.com>
2026-06-06 12:00:59 +1200

    Project Base

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ControllerWrangler.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
