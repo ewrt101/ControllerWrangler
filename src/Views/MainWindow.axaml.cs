3f954d807aa101c27e48162b4918ad050687be45
Ewrt <dan@radnewt.com>
2026-06-08 17:18:32 +1200

    moved src files to src folder

--- src/Views/MainWindow.axaml.cs
+++ src/Views/MainWindow.axaml.cs
@@ -1,31 +1,38 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Diagnostics;
+//using ControllerWrangler.Views;
 
 namespace ControllerWrangler.Views;
 
 public partial class MainWindow : Window
 {
+    //store Contentview
+    //private readonly UserControl _mainContentView = new MainContentView();
+    //private readonly UserControl _settingContentView = new UserControl();
+
     public MainWindow()
     {
         InitializeComponent();
+        // Set initial content to MainContentView6
+        //MainSplitView.Content = _mainContentView;
     }
 
     private void Pannel_button_Click(object sender, RoutedEventArgs e)
     {
         if (sender is Button button)
         {
-            string param = button.CommandParameter?.ToString();
+            string? param = button.CommandParameter?.ToString();
             Debug.WriteLine($"Button clicked: {param}");
             // Here you can add logic to switch the content based on the button clicked
             switch (param)
             {
                 case "Main":
-                    MainSplitView.Content = new MainContentView();
+                    //MainSplitView.Content = _mainContentView;
                     Debug.WriteLine("Switched to MainContentView");
                     break;
                 case "Settings":
-                    MainSplitView.Content = new SettingContentView();
+                    //MainSplitView.Content = _settingContentView;
                     Debug.WriteLine("Switched to SettingContentView");
                     break;
                 default:
